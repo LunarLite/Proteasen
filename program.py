0d75b46e02468bd12a6c0bdf5c002caa97452ea3
LunarLite <mdtozer@hotmail.com>
2017-11-06 14:01:37 +0100

    Added commandline arguement

--- program.py
+++ program.py
@@ -1,7 +1,19 @@
-class Aminozuur:
-	def __init__(self, type)
+# imports
+import sys
 
+
+# object that can be either a hydrophobic or polar
+class Aminozuur:
+	def __init__(self, type, x, y):
+		self.type = type
+		position_x = x
+		position_y = y
 
 # main execution
 if __name__ == '__main__':
 	# stuff to run
+	if (len(sys.argv) != 2):
+		sys.exit("Usage: python program.py HPPHHPPHH")
+
+	print("Given srting: " + str(sys.argv[1]))
+	
