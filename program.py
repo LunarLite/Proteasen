0d3496e219a706e559b46353ed53a0e9738ed398
LunarLite <mdtozer@hotmail.com>
2017-11-06 14:26:25 +0100

    dictOfMolecules added
    All molecules are now assigned  type and added to a dict

--- program.py
+++ program.py
@@ -4,16 +4,29 @@ import sys
 
 # object that can be either a hydrophobic or polar
 class Aminozuur:
-	def __init__(self, type, x, y):
-		self.type = type
-		position_x = x
-		position_y = y
+	def __init__(self, molecule_type):
+		self.molecule_type = molecule_type
 
 # main execution
 if __name__ == '__main__':
-	# stuff to run
+
+	# check given input
 	if (len(sys.argv) != 2):
 		sys.exit("Usage: python program.py HPPHHPPHH")
-
-	print("Given srting: " + str(sys.argv[1]))
+	input = str(sys.argv[1])
 	
+	dictOfMolecules = {}
+	count = 0
+	for c in input:
+		x = str(count)
+		count += 1
+		if (c.upper() == 'H'):
+			molecule_type = "hydrophobic"
+		else:
+			molecule_type = "polair"
+		dictOfMolecules[x] = Aminozuur(molecule_type)
+		
+	# check contents on name and type
+	for x in dictOfMolecules:
+		print str(x)
+		print str(dictOfMolecules[x].molecule_type)
