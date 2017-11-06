ee53b329eb017952ecdfe370c2cb7998fb155b2d
VanessaBotha <31652336+VanessaBotha@users.noreply.github.com>
2017-11-06 20:52:17 +0100

    List ipv dict & fold() aangemaakt
    Dictionary is vervangen door een list.
    
    Ik heb ipv dat het object Aminoacid x en y als input krijgt een functie fold() aangemaakt die de x en y coordinaten bepaald. Dit leek me makkelijk omdat we uiteindelijk als we een algoritme gaan schrijven dat dan meteen op die plek kunnen doen :)
    Ohja en ik heb een extra error check toegevoegt die controleert of de inhoud van de command line argument wel echt alleen maar H en P is. Die errorcheck staat nu in de load functie (misschien niet zo netjes ?) maar anders moet je bijna letterlijk die hele loadfunctiekopieren voor die errorcheck.

--- program.py
+++ program.py
@@ -4,82 +4,109 @@ from random import randint
 import sys
 
 # Global variables.
-dict_of_molecules = {}
+aminoacid_chain = []
 
-
-# Molecule. 
-# Can be either hydrophobic or polair, gets a position assigned based on the algorythm.
+# Aminoacid. 
+# Can be either hydrophobic or polair.
+# Gets a position assigned based on the algorythm.
 class Amino_acid:
-	def __init__(self, molecule_type, x, y):
+	def __init__(self, molecule_type):
 		self.molecule_type = molecule_type
-		self.x = x
-		self.y = y
+		self.x = 0
+		self.y = 0
 
+# Main function.
 def main():
 
-	# Check given input.
+	# Check if command line argument is given.
 	if (len(sys.argv) != 2):
 		sys.exit("Usage: python program.py HPPHHPPHH")
+
 	input = str(sys.argv[1])
 	
-	# Load the argv into molecule dict.
+	# Create amino acid chain by the given comment line argument.
 	load(input)
+
+	# Set x and y coordinates of the aminoacids of chain.
+	fold()
+
+	# Plot the "folded" aminoacid chain.
 	plot()
-	
 		
 	# Check contents on name and type.
-	for x in dict_of_molecules:
-		print str(x)
-		print str(dict_of_molecules[x].molecule_type)
+	for i in range(0, len(aminoacid_chain)):
+		print str(i)
+		print str(aminoacid_chain[i].molecule_type)
 
-# Loads aminoacid chain.
+
+# Creates a list of aminoacids by given comment line argument consisting of H and P.
+# Returns error when comment line argument contains characters other than H or P. 
 def load(input):
-	count = 0
+
+	# Iterate over each character in command line argument. 
 	for c in input:
-		x = str(count)
-		count += 1
+
+		# Allow only H and P in command line argument. 
+		# Set molecule type to either hydrophobic or polair, as H or P indicates.
 		if (c.upper() == 'H'):
 			molecule_type = "hydrophobic"
-		else:
-			molecule_type = "polair"
-		dict_of_molecules[x] = Amino_acid(molecule_type, randint(0, 9), randint(0, 9))
 
-# Plots optimal peptide chain configuration.
+		elif (c.upper() == 'P'):
+			molecule_type = "polair"
+
+		else: 
+			sys.exit("Usage: python program.py HPPHHPPHH")
+
+		# Create aminoacid with appropriate type and append it to the chain.
+		aminoacid_chain.append(Amino_acid(molecule_type))
+
+
+# Plots optimal aminoacid chain configuration.
 def plot():
 	
-	# Create empty list to store coordinates.
+	# Create empty lists to store x and y coordinates.
 	x = []
 	y = []
 
-	# Iterate over each aminoacid in dictionary. 
-	for i in dict_of_molecules:
+	# Iterate over each aminoacid. 
+	for i in range(0, len(aminoacid_chain)):
 
-		# Store coordinates of current aminoacid in list.
-		x.append(dict_of_molecules[i].x)
-		y.append(dict_of_molecules[i].y)
+		# Store x and y coordinates of current aminoacid.
+		x.append(aminoacid_chain[i].x)
+		y.append(aminoacid_chain[i].y)
 	
 	# Plot backbone aminoacid chain.
 	plt.plot(x, y, 'k-')
 
-	# Iterate over each aminoacid in dictionary.
-	for i in dict_of_molecules:
+	# Iterate over each aminoacid.
+	for i in range(0, len(aminoacid_chain)):
 		
 		# Check for type of current aminoacid. 
-		if dict_of_molecules[i].molecule_type == "hydrophobic": 
+		if aminoacid_chain[i].molecule_type == "hydrophobic": 
 
 			# Plot red dot at coordinates of hydrophobic aminoacid.
-			plt.plot(dict_of_molecules[i].x, dict_of_molecules[i].y, 'ro')
+			plt.plot(aminoacid_chain[i].x, aminoacid_chain[i].y, 'ro')
 
-		elif dict_of_molecules[i].molecule_type == "polair":  
+		elif aminoacid_chain[i].molecule_type == "polair":  
 
 			# Plot blue dot at coordinates of polair aminoacid.
-			plt.plot(dict_of_molecules[i].x, dict_of_molecules[i].y, 'bo')
+			plt.plot(aminoacid_chain[i].x, aminoacid_chain[i].y, 'bo')
 	
 	# Draw a grid behind plots. 
 	plt.grid()
 
 	# Display pop-up window with plot. 
 	plt.show()
+
+def fold(): 
+
+	#Iterate over each aminoacid.
+	for i in range(0, len(aminoacid_chain)): 
+
+		# set the x and y coordinate of current aminoacid.
+		# This has to be a better algorithm ... 
+		aminoacid_chain[i].x = randint(0, 9)
+		aminoacid_chain[i].y = randint(0, 9)
 
 		
 # Main execution
