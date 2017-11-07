7583ce69ff46618f3c2d2ec8ac12d97645993609
elinerietdijk <eline_rietdijk@hotmail.com>
2017-11-07 12:54:13 +0100

    nieuwe fold function

--- program.py
+++ program.py
@@ -102,13 +102,46 @@ def plot():
 def fold(): 
 
 	# Iterate over each aminoacid.
-	for i in range(0, len(aminoacid_chain)): 
+	for i in range (1, len(aminoacid_chain)):
 
-		# set the x and y coordinate of current aminoacid.
-		# This has to be a better algorithm ... 
-		aminoacid_chain[i].x = randint(0, 9)
-		aminoacid_chain[i].y = randint(0, 9)
+		# create array containing possible positions
+		prev_acid = aminoacid_chain[i - 1]
+		tup1 = [prev_acid.x + 1, prev_acid.y]
+		tup2 = [prev_acid.x - 1, prev_acid.y]
+		tup3 = [prev_acid.x, prev_acid.y + 1]
+		tup4 = [prev_acid.x, prev_acid.y - 1]
 
+		options = [tup1, tup2, tup3, tup4]
+
+		zelfde = True
+		fouten = []
+		fout = True
+
+		while zelfde:
+			zelfde = False
+
+			while fout:
+				option = randint(0, 3)
+				if option not in fouten:
+					fout = False
+
+			x = options[option][0]
+			y = options[option][1]
+
+			for j in range(0, i - 1):
+				if aminoacid_chain[j].x == x and aminoacid_chain[j].y == y:
+			 		zelfde = True
+			 		fouten.append(option)
+			 		break
+			 	else: 
+			 		zelfde = False
+			 		fouten = 0
+			 		
+
+		print(x, y)
+
+		aminoacid_chain[i].x = x
+		aminoacid_chain[i].y = y
 		
 # Main execution
 if __name__ == '__main__':
