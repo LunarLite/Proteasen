4f699503735e47e59924d8f22be2edf1a19f74a9
elinerietdijk <eline_rietdijk@hotmail.com>
2017-11-28 11:47:55 +0100

    hillclimber disabled

--- Application/Algorithms/hillclimber_algorithm.py
+++ Application/Algorithms/hillclimber_algorithm.py
@@ -9,20 +9,22 @@ best_random = []
 
 def fold(input):
 
-	global input_chain
-	input_chain = input
-	global best_random
-	random_list.append(copy.copy(random_algorithm.fold(input_chain)))
-	best_random = random_list[0]
+	# global input_chain
+	# input_chain = input
+	# global best_random
+	# random_list.append(copy.copy(random_algorithm.fold(input_chain)))
+	# best_random = random_list[0]
 
-	for i in range(1, 10):
-		random_list[i] = copy.copy(random_algorithm.fold(input_chain))
+	# for i in range(1, 10):
+	# 	random_list[i] = copy.copy(random_algorithm.fold(input_chain))
 
-		if random_list[i].stability() >= best_random.stability()
+	# 	if random_list[i].stability() >= best_random.stability():
 
 
 	# if chain stuck in conflict, set coordinates back and fold again
-	if output == 1:
-		for i in self.chain:
-			i.coordinates = [0, 0]
-		self.fold(algorithm)
+	# if output == 1:
+	# 	for i in self.chain:
+	# 		i.coordinates = [0, 0]
+	# 	self.fold(algorithm)
+
+	return input
