4edb5e01f724523512dba4e03109c8b7cc0b6420
LunarLite <mdtozer@hotmail.com>
2017-12-07 23:51:39 +0100

    Heuristics fixed
    Still needs to be cleaned up...

--- Application/Algorithms/breadthh_algorithm.py
+++ Application/Algorithms/breadthh_algorithm.py
@@ -1,5 +1,3 @@
-
-
 from Classes import AminoAcidChain
 from collections import deque
 import copy
@@ -10,9 +8,16 @@ import math
 input_chain = []
 chain_deque = deque()
 best_chain = []
-dynamic_length = 1
 best_score = 0
-best_chain = []
+
+# required domain calculation
+dynamic_length = 0
+
+# required score calculation
+passed_hydro = 0
+
+# statistics
+trashcan = 0
 
 
 def execute(input):
@@ -22,45 +27,52 @@ def execute(input):
 	input_chain = input.chain
 	input_chain[0].coordinates = [0,0]
 	input_chain[1].coordinates = [0,1]
+
+	
 	# determine x&y domain based on chain length
 	global dynamic_length
-	dynamic_length = math.floor(((math.sqrt(len(input_chain)))/3)*2)
+	#dynamic_length = math.ceil(math.sqrt(10))-math.floor(10/7)
+	dynamic_length = math.floor(math.sqrt(len(input_chain)))-1
+
 	# initialize starting chain of 2 nodes
 	start_chain = [input_chain[0], input_chain[1]]
 	# initialize start of the list
 	global chain_deque
 	chain_deque.append(start_chain)
-	# 'best_chain' is what needs to be returned.
-	global best_chain
-	best_chain = start_chain
 	
-	start = timeit.default_timer()
+
+
+	global passed_hydro
+	
 	# increase the size of the chains with 1 node every loop
 	for i in range (2, len(input_chain)):
-		print(i, "Heur")
-		# make sure all chains are the same (maximum) size
+		print(i+1)
+
 		while len(chain_deque[0]) < i + 1:
 			# pop chain from list
 			temp_chain = chain_deque.popleft()    
 			# check possible places for the new node
 			possibilities = checkPossibilities(temp_chain, i)
-			# build the new chain(s)
-			buildChain(temp_chain, i, possibilities)
+			# get possible builds
+			builds = formChain(temp_chain, i, possibilities)
+			# build new chains
+			buildChains(builds, i)				
+
+		if (input_chain[i].molecule_type == "hydrophobic"):
+			passed_hydro += 1
+
+	# temporary statistics
+	print("Chains thrown away: ", trashcan)
 			
-		end = timeit.default_timer()
-		
-	print("Time: ", (end-start))
+	# 'best_chain' is the chain that needs to be returned.
+	global best_chain
 	input.chain = best_chain
 	return input
 
-	
-	
-def buildChain(temp_chain, i, possibilities):
+def formChain(temp_chain, i, possibilities):
 
 	global input_chain
-	global chain_deque
-	global best_chain
-	global best_score
+	builds = []
 	
 	# make a new (possible) chain
 	for option in possibilities:
@@ -68,30 +80,54 @@ def buildChain(temp_chain, i, possibilities):
 		new_node = copy.copy(input_chain[i])
 		new_node.coordinates = option
 		new_chain.append(new_node)
-		chain_deque.append(new_chain)
+		builds.append(new_chain)
+
+	return builds
 	
+def buildChains(builds, i):
+
+	global chain_deque
+	global best_chain
+	global best_score
+	global passed_hydro
+	global trashcan
+	
+	for build in builds:
+		
 		# check the scores of both the old and new chain
 		new_acid_chain = AminoAcidChain.Amino_acid_chain("p")
-		new_acid_chain.chain = new_chain
+		new_acid_chain.chain = build
 		new_acid_chain.stability()
 		new_score = new_acid_chain.score
 		
 		if(new_score <= best_score):
-			best_chain = copy.copy(new_chain)
+			best_chain = copy.copy(build)
 			best_score = new_score
+		
+		# time to prune?
+		if (i % 10 == 0):
+			# what score should be pruned at
+			if (new_score <= -1*(passed_hydro/2)):
+				chain_deque.append(build)
+			else:
+				trashcan += 1
+		else:
+			chain_deque.append(build)
 			
 		
 # check possible positions a new node can be placed at
 def checkPossibilities(temp_chain, i):
 
 	global dynamic_length
-
+	
 	# remember coordinates of last amino acid in current chain
 	x = temp_chain[i - 1].coordinates[0]
 	y = temp_chain[i - 1].coordinates[1]
 	# create array containing possible positions
-	# options = [[x - 1, y], [x + 1, y], [x, y - 1], [x, y + 1]]
 	options = []
+
+		
+	# check for any new position if they fall within the allowed domain before adding
 	if(x - 1 >= -dynamic_length):
 		options.append([x - 1, y])
 	if(x + 1 <= dynamic_length):
@@ -101,24 +137,9 @@ def checkPossibilities(temp_chain, i):
 	if(y + 1 <= dynamic_length):
 		options.append([x, y + 1])
 	
-
 	# removes invalid options from the array
 	for j in temp_chain:
 		if j.coordinates in options:
 			options.remove(j.coordinates)
 	
 	return options
-	
-	
-# checks whether a chain already exists
-# def checkDuplicates(new_chain):
-	
-	
-	
-	
-	
-	
-	
-	
-
-
