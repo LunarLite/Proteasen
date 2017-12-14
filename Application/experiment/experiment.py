48a0adb8d599935c81db72f2c7a9432637914584
VanessaBotha <31652336+VanessaBotha@users.noreply.github.com>
2017-12-14 19:48:21 +0100

    experiment

--- Application/experiment/experiment.py
+++ Application/experiment/experiment.py
@@ -1,49 +1,50 @@
-#
 # experiment.py
 #
+# plots data from csv file of type: 
+# header 
+# x, y
+# x, y etc 
+#
 
-# plots aminoacid chain configuration
-def plot(self):
-	
-	# Add new subplot
-	subPlot = fig.add_subplot(111)
+import matplotlib.pyplot as plt 
+import csv
 
-	# create empty lists to store x and y coordinates
+fig = plt.figure()
+
+def main(): 
+
 	x = []
 	y = []
 
-	
-	# iterate over each aminoacid 
-	for i in range(0, len(self.chain)):
+	with open("test.csv", 'r') as csvfile:
+		reader = csv.reader(csvfile, delimiter=',')
 
-		# store x and y coordinates of current aminoacid
-		x.append(self.chain[i].coordinates[0])
-		y.append(self.chain[i].coordinates[1])
-	
-	# subplot backbone aminoacid chain
+		firstline = True
+		for row in reader: 
+			if firstline: 
+				firstline = False 
+				continue 
+			x.append(int(row[0]))
+			y.append(int(row[1]))
+
+		# print("x", x, "y", y)	
+
+		plot(x, y)
+
+
+def plot(x, y): 
+
+	# Add new subplot
+	subPlot = fig.add_subplot(111)
+
 	subPlot.plot(x, y, 'k-')
-	# set subplot ticks to the exact amount required
-	subPlot.set_xticks(x, False)
-	subPlot.set_yticks(y, False)
 
-	# iterate over each aminoacid
-	for i in range(0, len(self.chain)):
-		
-		# check for type of current aminoacid
-		if self.chain[i].molecule_type == "hydrophobic": 
-
-			# plot red dot at coordinates of hydrophobic aminoacid
-			subPlot.plot(self.chain[i].coordinates[0], self.chain[i].coordinates[1], 'ro')
-			plt.plot(self.chain[i].coordinates[0], self.chain[i].coordinates[1], 'ro')
-
-		elif self.chain[i].molecule_type == "polair":  
-
-			# plot blue dot at coordinates of polair aminoacid
-			subPlot.plot(self.chain[i].coordinates[0], self.chain[i].coordinates[1], 'bo')
-			plt.plot(self.chain[i].coordinates[0], self.chain[i].coordinates[1], 'bo')
-	
 	# draw a grid behind Subplot 
 	subPlot.grid()
 
 	# display pop-up window with plot
 	plt.show()
+
+
+if __name__ == '__main__':
+	main()
